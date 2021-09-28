47ee30ba0daec7de00d8aed682918a06abaa8ef8
ChukovAndrey <chukov.info@gmail.com>
2021-09-29 00:32:31 +0300

    feat: add routing - L19

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import style from './Navbar.module.css';
 
 const Navbar = () => {
 	return <nav className={style.navbar}>
-		<div><a href="#">Profile</a></div>
-		<div><a href="#">Messages</a></div>
+		<div><a href="/profile">Profile</a></div>
+		<div><a href="/dialogs">Messages</a></div>
 		<div><a href="#">News</a></div>
 		<div><a href="#">Music</a></div>
 		<div><a href="#">Settings</a></div>
