2880649de9ace58179ba916f719cfb6aa26826b1
ChukovAndrey <chukov.info@gmail.com>
2021-09-29 00:05:44 +0300

    feat: add Dialogs component - L18

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,12 +3,13 @@ import MyPosts from './MyPosts/MyPosts';
 import style from './Profile.module.css';
 
 const Profile = () => {
-	return <div className={style.content}>
+	return (
 		<div>
-			<img className={style.image} src="https://p.bigstockphoto.com/eIdTXLbqQilMs9xbjvcs_bigstock-Aerial-View-Of-Sandy-Beach-Wit-256330393.jpg" alt="content-img" />
-		</div>
-		<MyPosts />
-	</div>;
+			<div>
+				<img className={style.image} src="https://p.bigstockphoto.com/eIdTXLbqQilMs9xbjvcs_bigstock-Aerial-View-Of-Sandy-Beach-Wit-256330393.jpg" alt="content-img" />
+			</div>
+			<MyPosts />
+		</div>);
 }
 
 export default Profile;
