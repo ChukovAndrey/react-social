1ae0f0d30034e7274ac4b46592f7aac1f0f3e426
ChukovAndrey <chukov.info@gmail.com>
2021-11-10 00:46:45 +0300

    feat: L20 add navlinks to main menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from './Navbar.module.css';
 
 const Navbar = () => {
-	return <nav className={style.navbar}>
-		<div><a href="/profile">Profile</a></div>
-		<div><a href="/dialogs">Messages</a></div>
-		<div><a href="#">News</a></div>
-		<div><a href="#">Music</a></div>
-		<div><a href="#">Settings</a></div>
-	</nav>;
+	return (
+		<nav className={style.navbar}>
+			<div className={style.item}>
+				<NavLink to="/profile" activeClassName={style.activeLink}>Profile</NavLink>
+			</div>
+			<div className={style.item}>
+				<NavLink to="/dialogs" activeClassName={style.activeLink}>Messages</NavLink>
+			</div>
+			<div>
+				<a href="#">News</a>
+			</div>
+			<div>
+				<a href="#">Music</a>
+			</div>
+			<div>
+				<a href="#">Settings</a>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
