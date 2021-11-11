66849fc40b55390d5ea80be8be656345baaee3f3
ChukovAndrey <chukov.info@gmail.com>
2021-11-11 16:43:55 +0300

    refactor: change basic layout

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<div className="app-wrapper">
 				<Header />
-				<Navbar />
-				<div className="app-wrapper-content">
-					<Route path="/dialogs" component={Dialogs} />
-					<Route path="/profile" component={Profile} />
+				<div className="main-content-wrapper">
+					<Navbar />
+					<div className="app-wrapper-content">
+						<Route path="/dialogs" component={Dialogs} />
+						<Route path="/profile" component={Profile} />
+					</div>
 				</div>
+				<Footer />
 			</div>
 		</BrowserRouter>
 	);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import style from './Header.module.css';
 
 const Header = () => {
-	return <header className={style.header}>
-		<img className={style.logo} src="./logo192.png" alt="logo" />
-	</header>;
+	return (
+		<header className={style.header}>
+			<div className={style.headerWrapper}>
+				<div className={style.headerContent}>
+					<img className={style.logo} src="./logo192.png" alt="logo" />
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
